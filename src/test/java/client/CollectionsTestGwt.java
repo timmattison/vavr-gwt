410217266d8426b12afafe3e132c4d45845a3093
Tim Mattison <tim@mattison.org>
2021-04-28 15:33:34 -0400

    Revert "Switched to Gradle"
    This reverts commit f99dc1edf49c1619b18fe72ba5411866ddfd9bee.

--- src/test/java/client/CollectionsTestGwt.java
+++ src/test/java/client/CollectionsTestGwt.java
@@ -6,16 +6,19 @@
  */
 package client;
 
+import com.google.gwt.junit.client.GWTTestCase;
 import io.vavr.Function1;
 import io.vavr.Tuple;
 import io.vavr.Tuple1;
 import io.vavr.collection.*;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+public class CollectionsTestGwt extends GWTTestCase {
 
-public class CollectionsTestGwt {
-    @Test
+    @Override
+    public String getModuleName() {
+        return "TestModule";
+    }
+
     public void testTuple() {
         Tuple1<Integer> t = Tuple.of(1);
         assertEquals((int) t._1, 1);
@@ -23,57 +26,47 @@ public class CollectionsTestGwt {
     }
 
     private void applyCollection(Function1<char[], Traversable<Character>> factory) {
-        Traversable<Character> traversable = factory.apply(new char[]{'a', 'b', 'c'});
+        Traversable<Character> traversable = factory.apply(new char[] {'a', 'b', 'c'});
         assertEquals(traversable.count(i -> i != 'b'), 2);
     }
 
-    @Test
     public void testCompileArray() {
         applyCollection(chars -> Array.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileBitSet() {
         applyCollection(chars -> BitSet.withCharacters().ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileCharSeq() {
         applyCollection(chars -> CharSeq.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileHashSet() {
         applyCollection(chars -> HashSet.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileLinkedHashSet() {
         applyCollection(chars -> LinkedHashSet.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileList() {
         applyCollection(chars -> List.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompilePriorityQueue() {
         applyCollection(chars -> PriorityQueue.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileQueue() {
         applyCollection(chars -> Queue.ofAll(Iterator.ofAll(chars)));
     }
 
-    @Test
     public void testCompileTreeSet() {
         applyCollection(chars -> TreeSet.ofAll(Iterator.ofAll(chars)));
     }
 
     @SuppressWarnings("Convert2MethodRef")
-    @Test
     public void testCompileVector() {
         applyCollection(chars -> Vector.ofAll(chars));
     }
